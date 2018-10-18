b1db75bd0516bb0d00bb102e18f59e5d16710ae4
binggai xie <xiebinggai@outlook.com>
2018-10-18 17:56:49 +0800

    树

---
+++ LeetCode/Tree/BinaryTree.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Tree
+{
+    public class BinaryTree
+    {
+        /// <summary>
+        /// 递归处理
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            List<int> list = new List<int>();
+            if (root != null)
+            {
+                list.Add(root.val);
+                if (root.left != null)
+                {
+                    list.AddRange(PreorderTraversal(root.left));
+                }
+                if (root.right != null)
+                {
+                    list.AddRange(PreorderTraversal(root.right));
+                }
+            }
+            
+            return list;
+        }
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            List<int> list = new List<int>();
+            if (root != null)
+            {
+                if (root.left != null)
+                {
+                    list.AddRange(InorderTraversal(root.left));
+                }
+                list.Add(root.val);
+                if (root.right != null)
+                {
+                    list.AddRange(InorderTraversal(root.right));
+                }
+            }
+
+            return list;
+        }
+        public IList<int> PostorderTraversal(TreeNode root)
+        {
+            List<int> list = new List<int>();
+            if (root != null)
+            {
+                if (root.left != null)
+                {
+                    list.AddRange(PostorderTraversal(root.left));
+                }
+                
+                if (root.right != null)
+                {
+                    list.AddRange(PostorderTraversal(root.right));
+                }
+                list.Add(root.val);
+            }
+            return list;
+        }
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            List<IList<int>> list = new List<IList<int>>();
+            //List<TreeNode> nodes = new List<TreeNode>();
+            //if (root != null)
+            //{
+            //    nodes.Add(root);
+            //}
+            return list;
+        }
+    }
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+}
---
+++ XUnitTestProject1/BinaryTreeTest.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.Tree;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class BinaryTreeTest
+    {
+        [Fact]
+        public void PreorderTraversalTest()
+        {
+            TreeNode treeNode = new TreeNode(1);
+            treeNode.right = new TreeNode(2);
+            treeNode.right.left = new TreeNode(3);
+            var list=new BinaryTree().PreorderTraversal(treeNode);
+
+        }
+    }
+}
